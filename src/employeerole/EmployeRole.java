9b33303fe5da3aa651d9465d48e6017facab7abe
AbanobAdel656 <abanobadel656@gmail.com>
2025-10-19 11:44:40 +0300

    EmployeeRole has finished

---
+++ src/employeerole/EmployeRole.java
@@ -0,0 +1,98 @@
+
+package employeerole;
+import java.time.LocalDate;
+
+public class EmployeRole {
+    
+    private ProductDatabase PD;
+    private CustomerProductDatabase CPD;
+    
+public EmployeRole() 
+{
+        PD = new ProductDatabase("Products.txt");
+        CPD = new CustomerProductDatabase("CustomersProducts.txt");
+}
+        
+public void addProduct(String productID, String productName, String 
+manufacturerName, String supplierName, int quantity)
+{
+    if (contains(productID))
+    {
+        System.out.println("this Product: " + productName + "is Already exist.");
+        return;
+    }
+}
+    
+    public Product[] getListOfProducts() {
+    
+        return PD.returnAllRecords().toArray(new Product[0]);
+    }
+
+    public CustomerProduct[] getListOfPurchasingOperations() {
+        return CPD.returnAllRecords().toArray(new CustomerProduct[0]);
+    }
+    
+    
+     public boolean purchaseProduct(String customerSSN, String productID, LocalDate purchaseDate) {
+        Product p = PD.getRecord(productID);
+        if (p == null) return false;
+        if (p.getQuantity() <= 0) return false;
+        p.setQuantity(p.getQuantity() - 1);
+        
+        CustomerProduct cp = new CustomerProduct(customerSSN, productID, purchaseDate);
+        CPD.insertRecord(cp);
+        PD.saveToFile();
+        CPD.saveToFile();
+        return true;
+    }
+     
+     
+    public double returnProduct(String customerSSN, String productID, LocalDate purchaseDate ,LocalDate returnDate) 
+    {
+        if (returnDate.isBefore(purchaseDate)) return -1;
+        Product p = PD.getRecord(productID);
+        if (p == null) return -1;
+        String key = customerSSN + "," + productID + "," + purchaseDate.format(java.time.format.DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+        if (!CPD.contains(key)) return -1;
+        if (returnDate.isAfter(purchaseDate.plusDays(14)))
+            return -1;
+       // Otherwise -------------------------------------------------
+        p.setQuantity(p.getQuantity() + 1);
+        CPD.deleteRecord(key);
+        PD.saveToFile();
+        CPD.saveToFile();
+        return p.getPrice();
+    }
+    
+public boolean applyPayment(String customerSSN, LocalDate purchaseDate) {
+
+  ArrayList<CustomerProduct> records = CPD.returnAllRecords();
+
+    for (int i = 0; i < records.size(); i++) {
+        CustomerProduct cp = records.get(i);
+
+        if (cp.getCustomerSSN().equals(customerSSN) && cp.getPurchaseDate().equals(purchaseDate)) {
+            if (!cp.isPaid()) {
+                cp.setPaid(true);
+                CPD.saveToFile();
+                System.out.println("Paid successfully.");
+                return true; 
+            } 
+            else {
+                return false; 
+                System.out.println("This payment is already paid");
+            }
+        }
+    }
+
+        System.out.println("Invalid: There is no purchase !!");
+
+    return false;
+}
+
+    public void logout()
+     {
+      PD.saveToFile();
+      CPD.saveToFile();
+     }
+}
