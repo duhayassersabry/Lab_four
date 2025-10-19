0d52ee2e9a996458848a601c91dc82f4f76f4e30
doha yasser <duhasan2020@gmail.com>
2025-10-19 20:04:20 +0300

    Update Main.java

--- main/src/main/Main.java
+++ main/src/main/Main.java
@@ -1,21 +1,6 @@
 package main;
 public class Main {
  public static void main(String[] args) {
-    ProductDatabase db = new ProductDatabase("Products.txt");
-    db.readFromFile();
-    System.out.println("All records:");
-    for (Product p : db.returnAllRecords()) {
-        System.out.println(p.lineRepresentation());
-    public static void main(String[] args) {
-        
-    }
-    String key = "P2394";
-    System.out.println("Contains " + key + ": " + db.contains(key));
-    Product found = db.getRecord(key);
-    if (found != null) {
-        System.out.println("Found: " + found.lineRepresentation());
-    } else {
-        System.out.println("Not found: " + key);
-    }
+   
  }
 }
