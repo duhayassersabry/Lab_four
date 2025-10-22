f9859842f2bd184a9f01f935dfb9c8328dd21aac
Mariam2205 <mariammahmoudm25@gmail.com>
2025-10-22 19:44:15 +0300

    handling spaces

--- main/${test.src.dir}/CustomerProduct.java
+++ main/${test.src.dir}/CustomerProduct.java
@@ -8,12 +8,15 @@ public class CustomerProduct {
     private boolean paid;
 
         public CustomerProduct(String customerSSN, String productID, LocalDate purchaseDate) {
+    public CustomerProduct(String customerSSN, String productID, LocalDate purchaseDate) {
         this.customerSSN = customerSSN;
         this.productID = productID;
         this.purchaseDate = purchaseDate;
         this.paid = false;
     }
             public String getCustomerSSN() {
+
+    public String getCustomerSSN() {
         return customerSSN;
     }
 
@@ -25,20 +28,23 @@ public class CustomerProduct {
         return purchaseDate;
     }
         public boolean isPaid() {
+
+    public boolean isPaid() {
         return paid;
     }
 
     public void setPaid(boolean paid) {
         this.paid = paid;
     }
+
     public String lineRepresentation() {
         return customerSSN + "," + productID + "," +
                 String.format("%02d-%02d-%04d", purchaseDate.getDayOfMonth(), purchaseDate.getMonthValue(), purchaseDate.getYear()) +
                 "," + paid;
     }
+
     public String getSearchKey() {
         return customerSSN + "," + productID + "," +
                 String.format("%02d-%02d-%04d", purchaseDate.getDayOfMonth(), purchaseDate.getMonthValue(), purchaseDate.getYear());
     }
-
 }
