696799e6b61edc4109adb422090bcddc0053a4f3
Mariam2205 <mariammahmoudm25@gmail.com>
2025-10-22 18:47:55 +0300

    the constructor

--- main/${test.src.dir}/AdminRole.java
+++ main/${test.src.dir}/AdminRole.java
@@ -7,3 +7,12 @@ public class AdminRole {
         database.readFromFile();
     }
 
+    public void addEmployee(String employeeId, String name, String email, String address, String phoneNumber) {
+        if (!database.contains(employeeId)) {
+            EmployeeUser e = new EmployeeUser(employeeId, name, email, address, phoneNumber);
+            database.insertRecord(e);
+            database.saveToFile();
+        }
+    }
+
+  
